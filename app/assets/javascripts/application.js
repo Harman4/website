cbc0c8e63c758a6e0f9b33f00f3f3561a9de581e
Harman Kaur <timahrmn@gmail.com>
2018-07-10 13:58:55 -0700

    3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,7 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+//= required bootstrap
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
