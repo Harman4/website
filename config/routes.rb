a4a9724ccb8dc41359ab69bb9d83313cc473f04b
Harman Kaur <timahrmn@gmail.com>
2018-07-11 12:04:03 -0700

    4.1 Scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :products
+  resources :object_names
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'pages#home'
+root 'products#index'
+resources :charges
 end
